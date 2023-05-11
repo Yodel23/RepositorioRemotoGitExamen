a4b0d7402793985f335763526e28055f824da140
Yodel23 <dawjosele@gmail.com>
2023-05-11 18:24:29 +0200

    he creado un nuevo syso

---
+++ ProyectoGitExamen/src/paqueteGitExamen/SegundaClase.java
@@ -0,0 +1,12 @@
+package paqueteGitExamen;
+
+public class SegundaClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Primer mensaje por pantalla");
+		System.out.println("Segundo mensaje por pantalla");
+	}
+
+}
